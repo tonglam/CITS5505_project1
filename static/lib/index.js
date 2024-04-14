ad6e73ba294171ba0bed775b69f2d2a0beae2d4c
tong <bluedragon00000@gmail.com>
2024-04-15 02:38:36 +0800

    0414

---
+++ static/lib/index.js
@@ -0,0 +1,99 @@
+const lyrics = [
+  "Song for Hyperlinks",
+  "[Verse]",
+  "In the early days when the internet was new",
+  "A world of information",
+  "Right at our fingertips",
+  "From dial-up connections to high-speed broadband",
+  "The digital revolution",
+  "Changing the way we exist",
+  "(Click, click, type away)",
+  "Hyperlinks leading the way",
+  "(Scroll, scroll, find your place)",
+  "Discovering a whole new space",
+  "[Verse]",
+  "HTML",
+  "CSS",
+  "Building blocks of the web",
+  "Creating pages",
+  "Websites",
+  "A digital tapestry",
+  "JavaScript brought interactivity to life",
+  "With every click and hover",
+  "A new possibility",
+];
+
+$(document).ready(function () {
+  // 歌词走马灯
+
+  let totalWordCount = 0;
+
+  // concept section
+  totalWordCount += countElement(
+    "concept-word-count",
+    "concept-word-count-display"
+  );
+
+  // implementation section
+  totalWordCount += countElement(
+    "implement-word-count",
+    "implement-word-count-display"
+  );
+
+  // browers handle hyperlink section
+  totalWordCount += countElement(
+    "browers-handle-word-count",
+    "browers-handle-word-count-display"
+  );
+
+  // anchor tag section
+  totalWordCount += countElement(
+    "anchor-tag-word-count",
+    "anchor-tag-word-count-display"
+  );
+
+  // http section
+  totalWordCount += countElement("http-word-count", "http-word-count-display");
+
+  // ajax section
+  totalWordCount += countElement("ajax-word-count", "ajax-word-count-display");
+
+  // http request section
+  totalWordCount += countElement(
+    "http-request-word-count",
+    "http-request-word-count-display"
+  );
+
+  // http method section
+  totalWordCount += countElement("http-method-word-count", null);
+
+  // display total word count
+  const totalWordCountMessage = `Total words: ${totalWordCount}`;
+  const totalWordCountHtml = `<div class="text-info fs-5 mx-3">${totalWordCountMessage}</div>`;
+  document.getElementById("total-word-count-display").innerHTML =
+    totalWordCountHtml;
+});
+
+function countElement(id, displayId) {
+  let count = 0;
+  const texts = document.querySelectorAll(`#${id}`);
+  texts.forEach((text) => {
+    str = text.textContent;
+    count += countWord(str);
+  });
+  const wordCountMessage = `Words count by index.js: ${count}`;
+  const wordCountHtml = `<figcaption class="blockquote-footer text-info text-center fs-6 mt-1 px-5">${wordCountMessage}</figcaption>`;
+
+  if (document.getElementById(displayId) === null) {
+    return count;
+  }
+  document.getElementById(displayId).innerHTML = wordCountHtml;
+  return count;
+}
+
+function countWord(str) {
+  return str
+    .trim()
+    .split(/\s+/)
+    .filter((word) => word !== "").length;
+}
